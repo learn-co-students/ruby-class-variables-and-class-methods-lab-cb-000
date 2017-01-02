ce700c3fc7a9804a6fa514424869be50340a6d46
Jesus Vega <chuyspc@gmail.com>
2017-01-02 07:57:48 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,40 @@
 class Song
-  attr_accessor :artists :genres :genre_count
+  attr_accessor :artist, :genre, :genre_count
+
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  def initialize(name,artist,genre)
+    @@count += 1
+    @@genres << genre if !(genres.include?(genre))
+    @artist = artist
+    @genre = genre
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def count
+    @@count
+  end
+  
+  def artists
+    @@artists 
+  end
+
+  def genres
+    @@genres
+  end
+
+  def genre_count
+    @@genres.size
+  end
+
+  def artist_count
+    @@artists.size
+  end
+  
 end
