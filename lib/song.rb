ea4d926c09cde9328cf64c48ac96c9a638ff4035
Matthew <github email address>
2017-02-01 14:32:31 +0700

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,42 @@
+class Song
+	attr_reader :name, :artist, :genre
+
+	@@count = 0
+	@@artists = []
+	@@genres = []
+
+	def initialize(name, artist, genre)
+		@name = name
+		
+		@artist = artist
+		@@artists << @artist
+		
+		@genre = genre
+		@@genres << @genre
+		
+		@@count += 1
+	end 
+
+	def self.count 
+		@@count
+	end
+
+	def self.genres
+		@@genres.uniq
+	end
+
+	def self.artists
+		@@artists.uniq
+	end
+
+	def self.genre_count
+		g_count = {}
+		g_count = @@genres.inject(Hash.new(0)) { |h, i| h[i] += 1; h}
+		g_count
+	end
+
+	def self.artist_count
+		a_count = @@artists.inject(Hash.new(0)) { |h, i| h[i] += 1; h}
+	end
+
+end
