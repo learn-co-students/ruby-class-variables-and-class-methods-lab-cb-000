1dc0e0509176adbb2cd3cd69db73782e84d21250
Darnell Gillead <dlgillead@gmail.com>
2017-05-18 06:02:39 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,55 @@
+require 'pry'
+
 class Song
-  attr_accessor :name :artist :genre
+  attr_reader :name, :artist, :genre
 
+  @@count = 0
+  @@genres = []
+  @@artists = []
 
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+  end
+
+  def self.genres
+    @@genres = @@genres.uniq
+  end
+
+  def self.artists
+    @@artists = @@artists.uniq
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genre_count
+    genres_hash = {}
+    @@genres.each do |genre|
+      if genres_hash.include?(genre)
+        genres_hash[genre] += 1
+      else
+        genres_hash[genre] = 1
+      end
+    end
+    genres_hash
+  end
+
+  def self.artist_count
+    artists_hash = {}
+    @@artists.each do |artist|
+      if artists_hash.include?(artist)
+        artists_hash[artist] += 1
+      else
+        artists_hash[artist] = 1
+      end
+    end
+    artists_hash
+  end
 
 end
