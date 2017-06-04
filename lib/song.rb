fcee9af1f28f149d2a660aac635629ab7b0b2447
cptbrkfst <dyl@resonategroup.ca>
2017-06-03 21:44:40 -0400

    Completed lab

---
+++ lib/song.rb
@@ -0,0 +1,36 @@
+# Song class
+class Song
+  attr_accessor :name, :artist, :genre
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  def initialize(song_name, artist, genre)
+    @name = song_name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists = @@artists.uniq
+  end
+
+  def self.genres
+    @@genres = @@genres.uniq
+  end
+
+  def self.genre_count
+    @@genres.each_with_object(Hash.new(0)) { |key, value| value[key] += 1 }
+  end
+
+  def self.artist_count
+    @@artists.each_with_object(Hash.new(0)) { |key, value| value[key] += 1 }
+  end
+end
