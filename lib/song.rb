556b7abea8163a6fc0eb8b03233c30991ae8a7d1
Mark Fowler <fowlermark488@gmail.com>
2017-10-18 04:54:25 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,66 +1,54 @@
 class Song
 
-@@count = 0
-@@genres = []
-@@artists = []
+  attr_accessor :name, :artist, :genre
 
-attr_accessor :name, :artist, :genre
+  @@count = 0
+  @@genres = []
+  @@artists = []
 
-def initialize(name, artist, genre)
-  @name = name
-  @artist = artist
-  @genre = genre
-
-  @@count += 1
-
-  @@artists << artist
-  @@genres << genre
-
-end
-
-def self.count
-  @@count
-end
-
-def self.genres
-  genres = []
-  @@genres.each do |genre_type|
-    genres << genre_type if !genres.include?(genre_type)
-end
-  genres
-end
-
-def self.artists
-  artists = []
-  @@artists.each do |artist_name|
-    artists << artist_name if !artists.include?(artist_name)
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
   end
-  artists
-end
 
-def self.genre_count
-  genre_hash = {}
-  @@genres.each do |genre|
-    if genre_hash[genre] == nil
-      genre_hash[genre] = 1
-    else
-      genre_hash[genre] +=1
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1
+      else
+        genre_count[genre] = 1
+      end
     end
+    genre_count
   end
-    genre_hash
-end
 
-def self.artist_count
-  artist_hash = {}
-  @@artists.each do |artist|
-    if artist_hash[artist] == nil
-      artist_hash[artist] = 1
-    else
-      artist_hash[artist] +=1
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1
+      else
+        artist_count[artist] = 1
+      end
     end
+    artist_count
   end
-  artist_hash
-end
-
 
 end
