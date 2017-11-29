68b259c14d0a67bf7cd35bf48f375b2af72dd9b9
Jen Poole <contact.vandepool@gmail.com>
2017-11-29 02:24:48 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -57,6 +57,21 @@ class Song
     end
     return genre_count
   end
+
+  # Write a class method, .artist_count
+  # that returns a hash in which the keys are the names of each artist, and the value is the number of songs that have that artist.
+  def self.artist_count
+    artist_count = {}
+
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1
+      else
+        artist_count[artist] = 1
+      end
+    end
+    return artist_count
+  end
 end
 
 
