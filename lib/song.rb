46092ef45ad3f902010449bdfb4e35bf95481d0b
Jen Poole <contact.vandepool@gmail.com>
2017-11-29 02:08:16 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -3,6 +3,8 @@ class Song
 
   # keep track of the number of new songs that are created
   @@count = 0
+  # keep track of the genres of songs
+  @@genres = []
 
   # an individual song is initialized with a name, artist and genre
   def initialize (name, artist, genre)
@@ -12,10 +14,25 @@ class Song
 
     # increment count of songs when a new song is added
     @@count += 1
+    # add the genre of the song when a new song is added
+    @@genres << genre
   end
 
   # Write a class method, .count, that returns the total number of songs created
   def self.count
     @@count
   end
+
+  # Write a class method, .genres that returns an array of all of the genres of existing songs.
+  # This array should contain only unique genres, no duplicates
+  def self.genres
+    @@genres.uniq
+  end
 end
+
+
+=begin
+Check an array for duplicates
+https://stackoverflow.com/questions/4351390/how-do-i-check-an-array-for-duplicates
+
+=end
