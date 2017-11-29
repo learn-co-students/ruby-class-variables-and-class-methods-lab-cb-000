9157e76f274c5d5a033f3e6e9cbe1e5cdf5b4fd8
Jen Poole <contact.vandepool@gmail.com>
2017-11-29 02:01:40 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,21 @@
 class Song
   attr_accessor :name, :artist, :genre
 
+  # keep track of the number of new songs that are created
+  @@count = 0
+
   # an individual song is initialized with a name, artist and genre
   def initialize (name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
+
+    # increment count of songs when a new song is added
+    @@count += 1
+  end
+
+  # Write a class method, .count, that returns the total number of songs created
+  def self.count
+    @@count
   end
 end
