100518511a4c71c26f12208d3cd4c722a29fa373
David Hopkins <dlhopkins2@gmail.com>
2018-01-29 02:48:26 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,43 +1,52 @@
 class Song
   attr_accessor :name, :artist, :genre
-  @@artist_count = {}
-  @@genre_count = {}
 
   @@count = 0
   @@artists = []
   @@genres = []
 
   def initialize(name, artist, genre)
-      @name = name
-      @artist = artist
-      @genre = genre
-
-      @@genre_count.keys.include?(genre) ? @@genre_count[genre] += 1  : @@genre_count[genre] = 1
-      @@artist_count.keys.include?(artist) ? @@artist_count[artist] += 1  : @@artist_count[artist] = 1
-
-      @@artists << artist
-      @@genres << genre
-      @@count += 1
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count +=1
+    @@genres << genre
+    @@artists << artist
   end
 
   def self.count
-    return @@count
+    @@count
   end
 
   def self.artists
-    return @@artists.uniq
-  end
-
-  def self.genres
-    return @@genres.uniq
-  end
-
-  def self.genre_count
-    return @@genre_count
+    @@artists.uniq
   end
 
   def self.artist_count
-    return @@artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1
+      else
+        artist_count[artist] = 1
+      end
+    end
+    artist_count
   end
 
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
 end
